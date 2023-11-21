956ac0507bb79cd72849e232144e67afa2968eb2
Patryk Barański <pbaranski740@gmail.com>
2023-11-21 22:01:44 +0100

    Complete App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const startTimerHanlder = () => {
+    setIsRunning(true);
+  };
+
+  const resetTimerHandler = () => {
+    setTime(0);
+  };
+
+  const stopTimerHandler = () => {
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevState) => prevState + 1);
+      }, 1);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [time, isRunning]);
+
+  const convertMsToTimeString = (ms) => {
+    return new Date(ms).toISOString().slice(11, 22);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles["main-container"]}>
+      <p className={styles["counter"]}>{convertMsToTimeString(time)}</p>
+      <div className={styles["button-container"]}>
+        <Button onClick={startTimerHanlder}>Start</Button>
+        <Button onClick={stopTimerHandler}>Stop</Button>
+        <Button onClick={resetTimerHandler}>Reset</Button>
+      </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import styles from "./Button.module.scss";
+
+function Button({ onClick, children }) {
+  return (
+    <button className={styles.button} type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
